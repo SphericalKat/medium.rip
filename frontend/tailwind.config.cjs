ce73ad4dfc37a3353eb11c009ec2d24a41559cce
Sphericalkat <amolele@gmail.com>
2023-05-29 00:21:34 +0530

    feat: add dark mode
    Signed-off-by: Sphericalkat <amolele@gmail.com>

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.ts", "./**/*.html"],
+  darkMode: 'class',
   theme: {
     fontFamily: {
         'sans': ['Inter'],
