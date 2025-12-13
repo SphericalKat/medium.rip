b5784f609e3d2850cdc533dcd8db9c9cc72561c5
mycodedoesnotcompile2 <149587341+mycodedoesnotcompile2@users.noreply.github.com>
2025-12-13 12:45:06 +0000

    Update medium_client.go

--- pkg/client/medium_client.go
+++ pkg/client/medium_client.go
@@ -37,8 +37,8 @@ func PostData(postId string) (*entities.MediumResponse, error) {
 	
 	payload := strings.NewReader(fmt.Sprintf("{\"query\":\"query {\\n        post(id: \\\"%s\\\") {\\n          title\\n          createdAt\\n          creator {\\n            id\\n            name\\n          }\\n          content {\\n            bodyModel {\\n              paragraphs {\\n                name\\n                text\\n                type\\n                href\\n                layout\\n                markups {\\n                  title\\n                  type\\n                  href\\n                  userId\\n                  start\\n                  end\\n                  anchorType\\n                }\\n                iframe {\\n                  mediaResource {\\n                    href\\n                    iframeSrc\\n                    iframeWidth\\n                    iframeHeight\\n                  }\\n                }\\n                metadata {\\n                  id\\n                  originalWidth\\n                  originalHeight\\n                }\\n              }\\n            }\\n          }\\n        }\\n      }\",\"variables\":{}}", postId))
 
-	log.Printf("Article ID: %s", postId)
-	log.Printf("PAYLOAD: %s", payload)
+	//log.Printf("Article ID: %s", postId)
+	//log.Printf("PAYLOAD: %s", payload)
 
 	var client *http.Client
 
@@ -55,7 +55,6 @@ func PostData(postId string) (*entities.MediumResponse, error) {
 				},
 			},
 		}
-		log.Printf("Using proxy: %s", config.Conf.Proxy)
 	} else {
 		client = &http.Client{}
 	}
