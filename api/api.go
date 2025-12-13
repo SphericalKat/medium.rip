07949db7d54f6eca619cbe6683039538949b7fe4
mycodedoesnotcompile2 <149587341+mycodedoesnotcompile2@users.noreply.github.com>
2025-12-13 11:33:12 +0000

    Update api.go

--- api/api.go
+++ api/api.go
@@ -35,7 +35,7 @@ func RegisterRoutes(ctx context.Context, wg *sync.WaitGroup, engine *html.Engine
 	}))
 
 	if config.Conf.Proxy != "" {
-		proxy.WithTlsConfig(&tls.Config{ InsecureSkipVerify: false,})
+		proxy.WithTlsConfig(&tls.Config{ InsecureSkipVerify: true,})
 		app.Use(proxy.Balancer(proxy.Config{ Servers: []string{ config.Conf.Proxy, } , }))
 		log.Printf("Using proxy: %s", config.Conf.Proxy)
 	}
