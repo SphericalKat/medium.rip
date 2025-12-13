4ae0b3c242bbad7a41b96cd5313302c001016c1a
mycodedoesnotcompile2 <149587341+mycodedoesnotcompile2@users.noreply.github.com>
2025-12-13 11:50:04 +0000

    Update api.go

--- api/api.go
+++ api/api.go
@@ -41,15 +41,16 @@ func RegisterRoutes(ctx context.Context, wg *sync.WaitGroup, engine *html.Engine
 			ModifyRequest: func(c *fiber.Ctx) error {
 				//c.Request().Header.Add("X-Real-IP", c.IP())
 				//return nil
-				req_code, req_body, req_errs := c.Request().String()
-				log.Printf("RESPONSE: %s | %s | %s ", req_code, req_body, req_errs)
+				//req_code, req_body, req_errs := c.Request().String()
+				//log.Printf("RESPONSE: %s | %s | %s ", req_code, req_body, req_errs)
+				log.Printf("REQUEST: %s", c.Request().Body())
 				return nil
 			},
 			ModifyResponse: func(c *fiber.Ctx) error {
 				//c.Response().Header.Del(fiber.HeaderServer)
 				//return nil
-				res_code, res_body, res_errs := c.Response().String()
-				log.Printf("RESPONSE: %s | %s | %s ", res_code, res_body, res_errs)
+				//res_code, res_body, res_errs := c.Response().String()
+				//log.Printf("RESPONSE: %s | %s | %s ", res_code, res_body, res_errs)
 				return nil
 			},
 		}))
